0ef40645fcd54ac6a269b616de48a4dd01762b08
Tim <timotheecronier@gmail.com>
2022-12-25 11:20:49 +0100

    create article

--- app/pages/articles/[id].js
+++ app/pages/articles/[id].js
@@ -119,21 +119,7 @@ export default function Article({ post }) {
                   <textarea value={newCom || ''} onChange={(e) => setNewCom(e.target.value)} class="writeComment"></textarea>
                   <button onClick={() => postComment()} class="submitForm"><span>Post comment</span></button>
                </div>
-            </div>
-            <div class="writeCommentWrapper">
-               <span style={{fontSize:'30px', fontWeight:'600'}}>Title :</span>
-               <textarea onChange={(e) => (setChar1(e.target.value.length + " (max 100)"))} maxlength='100' class="writeArticleTitle"></textarea>
-               <p class="countChar">{char1}</p>
-               <br/><br/>
-               <span style={{fontSize:'30px', fontWeight:'600'}}>Content :</span>
-               <br/>
-               <textarea onChange={(e) => (setChar2(e.target.value.length + " (max 1000)"))} maxlength='1000' class="writeArticleContent"></textarea>
-               <p class="countChar">{char2}</p>
-               <br/><br/>
-               <span style={{fontSize:'30px', fontWeight:'600'}}>Image source :</span>
-               <textarea class="writeArticleTitle"></textarea>
-               
-            </div>  
+            </div> 
          </div >
       )
    } else {
@@ -164,9 +150,7 @@ export default function Article({ post }) {
 
             <div class="writeComment">
 
-            </div>{comment.author_id === session.user.id ?
-                              <button class="commentsAuthorEditImage" ><img src="https://cdn-icons-png.flaticon.com/512/3756/3756522.png" ></img></button>
-                              : <></>}
+            </div>
          </div >
       )
    }
---
+++ app/pages/createArticle.js
@@ -0,0 +1,60 @@
+import { useSession } from "@supabase/auth-helpers-react"
+import { useState, useEffect } from "react"
+import supabase from "../../utils/supabase"
+
+
+export default function CreateArticle() {
+    const session = useSession()
+    const [content, setContent] = useState()
+    const [title, setTitle] = useState()
+    const [source, setSource] = useState()
+
+    async function postArticle() {
+        try {
+            await supabase.from('articles').insert({
+                author_id: session.user.id,
+                content: content,
+                title: title,
+                source: source
+            })
+            alert("Your article is uploaded !")
+        } catch (error) {
+            console.log(error)
+        }
+        setContent(null)
+        setTitle(null)
+        setSource(null)
+    }
+
+    return (
+        <div class="globalWrapper">
+            <div class="writeCommentWrapper">
+                <span style={{ fontSize: '30px', fontWeight: '600' }}>Title :</span>
+                <textarea onChange={(e) => {
+                    (
+                        setChar1(e.target.value.length + " (max 100)"));
+                    setTitle(e.target.value);
+                }}
+                    maxlength='100'
+                    class="writeArticleTitle"></textarea>
+                <p class="countChar">{char1}</p>
+                <br /><br />
+                <span style={{ fontSize: '30px', fontWeight: '600' }}>Content :</span>
+                <br />
+                <textarea onChange={(e) => {
+                    (
+                        setChar2(e.target.value.length + " (max 1000)"));
+                    setContent(e.target.value);
+                }}
+                    maxlength='1000'
+                    class="writeArticleContent"></textarea>
+                <p class="countChar">{char2}</p>
+                <br /><br />
+                <span style={{ fontSize: '30px', fontWeight: '600' }}>Image source :</span>
+                <textarea onChange={(e) => setSource(e.target.value)} class="writeArticleTitle"></textarea>
+                <br />
+                <button onClick={(e) => { postArticle() }} class="submitForm"></button>
+            </div>
+        </div>
+    )
+}
