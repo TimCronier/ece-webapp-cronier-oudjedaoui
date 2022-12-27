e731f5b3a86269304589e93efdb5fcb2af7ba73d
Tim <timotheecronier@gmail.com>
2022-12-28 00:16:06 +0100

    fix footer

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer(props) {
    return (
       <div class={!props.darkMode ? "darkText darkBg footDark" : " footLight lightText lightBg"}>
          <p>
-            Site créé par Timothée Cronier et Sami Ouedjani
+            Site créé par Timothée Cronier et Sami Oudjedaoui
          </p>
       </div>
    )
