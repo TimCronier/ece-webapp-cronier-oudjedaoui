c8bcc44bf5905d03fea65866584ae222672a9342
Tim <timotheecronier@gmail.com>
2022-12-27 15:52:57 +0100

    fix error

--- app/pages/contacts.js
+++ app/pages/contacts.js
@@ -1,11 +1,10 @@
-import zebi from "../Components/zebi"
 
 export default function Contacts() {
    return (
       <div id="list" style = {{minHeight: '81.3vh'}}>
          <p>Contacts</p>
          <ul>
-             <zebi username="wsh">d</zebi> 
+            
             <li>Timothée Cronier - <strong>timothee.cronier@edu.ece.fr</strong></li>
             <li>Sami Ouedjani - <strong>sami.ouedjani@edu.ece.fr</strong></li>
          </ul>
