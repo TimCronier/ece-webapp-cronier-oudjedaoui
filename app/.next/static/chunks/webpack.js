a56253367deeaf348a72c42fc1769fb1a2ddbaff
Tim <timotheecronier@gmail.com>
2022-12-27 18:44:16 +0100

    fix error 3

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d2a2b701a55f5d5"; }
+/******/ 		__webpack_require__.h = function() { return "30bb2300a1da880a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
